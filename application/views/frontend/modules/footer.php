a14cd3d55973bedcea23376e2cf80a93710afc70
nguyenbach2704 <nguyenbach27042000@gmail.com>
2022-06-06 22:34:16 +0700

    bach2704

---
+++ application/views/frontend/modules/footer.php
@@ -0,0 +1,60 @@
+<footer id="footer">
+    <div class="news-social">
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <ul class="list-unstyled social pull-right">
+                  <li><a href="https://www.facebook.com/coxoamot.nucuoi/"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="https://twitter.com/dhti12a1n"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                  <li><a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a></li>
+              </ul>
+          </div>
+      </div>
+  </div>
+  <div class="top-footer">
+    <div class="container">
+        <div class="col-xs-12 col-sm-12 col-md-6">
+            <div class="col-xs-12 col-sm-6">
+                <a href="trang-chu">
+                    <img src="public/images/smart-mobi2.png" style="width: 100%;" class="img-fix f-logo" alt="smart-construction">
+                </a>
+                <div class="about-store">
+             
+             </div>
+         </div>
+         <div class="col-xs-12 col-sm-6">
+            <div class="f-contact">
+                <h3>Thông tin liên hệ</h3>
+                <ul class="list-unstyled">
+                    <li class="clearfix">
+                        <i class="fa fa-map-marker"></i>
+                        <span>273-Đường Lộc Vượng tp.Nam Định</span>
+                    </li>
+                    <li class="clearfix">
+                        <i class="fa fa-phone"></i>
+                        <span>037.501.3140 - 037.961.6040</span>
+                    </li>
+                    <li class="clearfix">
+                        <i class="fa fa-envelope"></i>
+                        <span><a href="mailto:vanhiepp1998@gmail.com">nguyenbach27042000@gamil.com</a></span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6">
+        <div class="col-xs-12 col-sm-6">
+            <h3 class="footer-title">HỖ TRỢ KHÁCH HÀNG</h3>
+            <ul class="list-unstyled linklists">
+                <li><a href="chinh-sach">Hướng dẫn thanh toán</a></li>          
+                <li><a href="chinh-sach">Hướng dẫn đặt hàng</a></li>
+                <li><a href="dieu-khoan">Điều khoản</a></li>                   
+            </ul>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+           <!-- <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=200&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" width="340" height="200" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe> -->
+       </div>
+   </div>
+</div>
+</div>
+</footer>
